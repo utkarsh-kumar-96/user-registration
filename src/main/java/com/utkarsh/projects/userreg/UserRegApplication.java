c1c799797f52cf4f35b3ca9dae1c8c46586f462d
utkarsh-kumar-96 <utkarshkumar199626@gmail.com>
2022-11-13 13:22:03 +0530

    init

---
+++ src/main/java/com/utkarsh/projects/userreg/UserRegApplication.java
@@ -0,0 +1,13 @@
+package com.utkarsh.projects.userreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utkarsh/projects/userreg/UserRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.projects.userreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
